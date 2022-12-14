2422936fd3e11cbf6367f767b1e27864cd719a49
Yuvenal Njoroge <yuvenal.njoroge@gmail.com>
2022-12-14 16:32:33 +0300

    switch to es6 import

--- string.js
+++ string.js
@@ -1,18 +1,16 @@
-const stringLength = string => {
+export const stringLength = string => {
   if (string.length < 1) {
     throw new Error('String has less than one character')
   }
   return string.length
 }
 
-const reverseString = string => {
+export const reverseString = string => {
   return [...string].reduce((x, y) => y.concat(x));
 }
 
-const capitalize = string => {
+export const capitalize = string => {
   const first = string.charAt(0).toUpperCase()
   const otherLetters = string.slice(1)
   return first.concat(otherLetters)
 }
-
-module.exports = { stringLength, reverseString, capitalize }
--- string.test.js
+++ string.test.js
@@ -1,26 +1,25 @@
-const str = require('./string')
+import { stringLength, reverseString, capitalize } from "./string.js"
 const value = 'random string'
 // const value = ''
 
 describe('stringLength module', () =>{
   test('length of value is atleast 1', () =>{
-    expect(str.stringLength(value)).not.toBe(0)
+    expect(stringLength(value)).not.toBe(0)
   })
 
   test('length of value is equal to 13', () =>{
-    expect(str.stringLength(value)).toBe(13)
+    expect(stringLength(value)).toBe(13)
   })
 })
 
 describe('reverseString module', () => {
   test('Reverse the string',()=>{
-    expect(str.reverseString(value)).toBe('gnirts modnar')
+    expect(reverseString(value)).toBe('gnirts modnar')
   })
 })
 
 describe('Capitalize module', () =>{
   test('Capitalize first letter',() =>{
-    expect(str.capitalize(value)).toBe('Random string')
+    expect(capitalize(value)).toBe('Random string')
   })
 })
-
