5bfdd3169d017b9e515f6bf4ea045dcbd75e2a1f
Yuvenal Njoroge <yuvenal.njoroge@gmail.com>
2022-12-14 14:38:47 +0300

    add reverse strring function

--- string.js
+++ string.js
@@ -1,10 +1,12 @@
 const stringLength = string => {
-
   if (string.length < 1) {
     throw new Error('String has less than one character')
   }
-  
   return string.length
 }
 
-module.exports = stringLength
+const reverseString = string => {
+  return [...string].reduce((x, y) => y.concat(x));
+}
+
+module.exports = { stringLength, reverseString }
